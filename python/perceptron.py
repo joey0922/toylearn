2a981add7c32f4eb3eeee40cfd62ffc12d82715c
Joey.Hu <zjuhjj@126.com>
2020-06-24 23:43:14 +0800

    update percptron code

--- python/perceptron.py
+++ python/perceptron.py
@@ -4,11 +4,17 @@
 import numpy as np
 from copy import deepcopy
 
+'''
+优点：简单、易于实现
+缺点：有无数个解
+'''
+
+
 # simple implement of perceptron algorithm
 class Perceptron:
 
     def __init__(self, learning_rate=1.0, random_state=0, dual_form=False,
-                 batch_size=1, n_iter_no_change=5, max_iter=1000, shuffle=True):
+                 max_iter=1000, shuffle=True):
 
         if not isinstance(learning_rate, (int, float)) or not 0 < learning_rate <= 1:
             raise ValueError('Learning rate is numeric greater than 0 '
@@ -23,14 +29,6 @@ class Perceptron:
         if not isinstance(dual_form, bool):
             raise ValueError("Type of parameter 'dual_form' must be bool.")
         self.__dual_form = dual_form
-
-        if not isinstance(batch_size, int) or batch_size < 1:
-            raise ValueError('Batch size must be an int number and greater than 0.')
-        self.__batch_size = batch_size
-
-        if not isinstance(n_iter_no_change, int) or n_iter_no_change <= 1:
-            raise ValueError("'n_iter_no_change' must an int number and greater than 1.")
-        self.__n_iter_no_change = n_iter_no_change
 
         if not isinstance(max_iter, int) or max_iter <= 1:
             raise ValueError('Max iteration must be an int number and greater than 1.')
@@ -64,14 +62,6 @@ class Perceptron:
         return self.__dual_form
 
     @property
-    def batch_size(self):
-        return self.__batch_size
-
-    @property
-    def n_iter_no_change(self):
-        return self.__n_iter_no_change
-
-    @property
     def max_iter(self):
         return self.__max_iter
 
@@ -98,18 +88,6 @@ class Perceptron:
         if not isinstance(df, bool):
             raise ValueError("Type of parameter 'df' user input is not bool. ")
         self.__dual_form = df
-
-    @batch_size.setter
-    def batch_size(self, bs):
-        if not isinstance(bs, int) or bs < 1:
-            raise ValueError('Batch size must be an int number and greater than 0.')
-        self.__batch_size = bs
-
-    @n_iter_no_change.setter
-    def n_iter_no_change(self, ninc):
-        if not isinstance(ninc, int) and ninc <= 0:
-            raise ValueError("'n_iter_no_change' must an int number and greater than 1.")
-        self.__n_iter_no_change = ninc
 
     @max_iter.setter
     def max_iter(self, mi):
@@ -145,100 +123,68 @@ class Perceptron:
     # 原始形式
     def __origin_fit(self, train_set, labels):
 
-        w, b = self.__random_state, self.__random_state
-        tmp_w, tmp_b = self.__random_state, self.__random_state
-        delta_w, delta_b = 0, 0
-
         idx_list = np.array([i for i in range(labels.shape[0])])
         if self.__shuffle:
             np.random.shuffle(idx_list)
         length = idx_list.shape[0]
 
-        loop_index, no_change_turn = 0, 0
-        i, err_batch = 0, 0
+        w, b = self.__random_state, self.__random_state
+
+        loop_index = 0
+        i, pos_i = 0, 0
         while True:
             loop_index += 1
 
             idx = idx_list[i]
             if labels[idx] * ((w * train_set[idx]).sum() + b) <= 0:
-                delta_w += self.__learning_rate * labels[idx] * train_set[idx]
-                delta_b += self.__learning_rate * labels[idx]
-                err_batch += 1
-            if err_batch == self.__batch_size:
-                w += delta_w / self.__batch_size
-                b += delta_b / self.__batch_size
-                delta_w, delta_b, err_batch = 0, 0, 0
-
-            if (tmp_w == w).all() and tmp_b == b:
-                no_change_turn += 1
+                w += self.__learning_rate * labels[idx] * train_set[idx]
+                b += self.__learning_rate * labels[idx]
+                pos_i = 0
             else:
-                tmp_w, tmp_b = deepcopy(w), b
-                if no_change_turn != 0:
-                    no_change_turn = 0
-            if no_change_turn == self.__n_iter_no_change:
+                pos_i += 1
+            if pos_i == length:
                 break
 
             if loop_index == self.__max_iter:
                 break
 
-            i += 1
-            if i == length:
-                i = 0
-                if self.__shuffle:
-                    np.random.shuffle(idx_list)
+            i = (0, i+1)[i < length - 1]
 
         return w, b
 
     def __dual_fit(self, train_set, labels):
-
-        gram_matrix = np.dot(train_set, train_set.T)
-
-        alpha = np.array([self.__random_state]*labels.shape[0]).astype('float64')
-        w = sum((alpha * labels).reshape(-1, 1) * train_set)
-        b = self.__random_state
-
-        tmp_w, tmp_b = deepcopy(w), self.__random_state
-        delta_alpha, delta_b = 0, 0
 
         idx_list = np.array([i for i in range(labels.shape[0])])
         if self.__shuffle:
             np.random.shuffle(idx_list)
         length = idx_list.shape[0]
 
-        loop_index, no_change_turn = 0, 0
-        i, err_batch = 0, 0
+        gram_matrix = np.dot(train_set, train_set.T)
+        alpha = np.array([self.__random_state]*labels.shape[0]).astype('float64')
+        b = self.__random_state
+
+        loop_index = 0
+        i, pos_i = 0, 0
         while True:
             loop_index += 1
-            print(loop_index, w, b)
+            print(loop_index, alpha, b)
 
             idx = idx_list[i]
             if labels[idx] * (np.dot(alpha*labels, gram_matrix[:, idx]) + b) <= 0:
-                delta_alpha += self.__learning_rate
-                delta_b += self.__learning_rate * labels[idx]
-                err_batch += 1
-            if err_batch == self.__batch_size:
-                alpha += delta_alpha / self.__batch_size
-                b += delta_b / self.__batch_size
-                delta_alpha, delta_b, err_batch = 0, 0, 0
-
-            w = sum((alpha * labels).reshape(-1, 1) * train_set)
-            if (tmp_w == w).all() and tmp_b == b:
-                no_change_turn += 1
+                alpha[idx] += self.__learning_rate
+                b += self.__learning_rate * labels[idx]
+                pos_i = 0
             else:
-                tmp_w, tmp_b = deepcopy(w), b
-                if no_change_turn != 0:
-                    no_change_turn = 0
-            if no_change_turn == self.__n_iter_no_change:
+                pos_i += 1
+            if pos_i == length:
                 break
 
             if loop_index == self.__max_iter:
                 break
 
-            i += 1
-            if i == length:
-                i = 0
-                if self.__shuffle:
-                    np.random.shuffle(idx_list)
+            i = {False: 0, True: i + 1}[i < length - 1]
+
+        w = sum((alpha * labels).reshape(-1, 1) * train_set)
 
         return w, b
 
@@ -267,9 +213,7 @@ if __name__ == '__main__':
 
     data_set = np.array([[3, 3], [4, 3], [1, 1]])
     label_set = np.array([1, 1, -1])
-    tron = Perceptron(dual_form=True, max_iter=1000)
+    alpha = np.array([2,0,5])
+    tron = Perceptron(dual_form=True, shuffle=False)
     model = tron.fit(data_set, label_set)
     print(model.coef_, model.bias)
-    # gram = np.dot(data_set, data_set.T)
-    # print(1 * label_set*gram[0, :])
-    # print(np.dot(1 * label_set, gram[0, :]))
